d6e00a796bcd78152acb14b6f9adc3142d608c8e
AmritHimanshu <amrit.ewp@gmail.com>
2023-12-21 14:32:13 +0530

    added dotenv

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-const DB = `mongodb+srv://portfolio:portfolio@cluster0.kargifk.mongodb.net/?retryWrites=true&w=majority`;
-
-mongoose.connect(DB).then(() => {
-    console.log("Connection successful");
-}).catch((err) => console.log('No connection'));
+dotenv.config({ path: './config.env' });
+require('./db/conn');
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello world from the server");
 });
 
-app.listen(3000, () => {
-    console.log(`Server is running at port no 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running at port no ${PORT}`);
 });
