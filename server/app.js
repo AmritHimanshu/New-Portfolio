c16cae5e853e182f9321d964d8d817f2d6ffd9d4
AmritHimanshu <amrit.ewp@gmail.com>
2023-12-21 14:08:05 +0530

    established connection to database

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+const DB = `mongodb+srv://portfolio:portfolio@cluster0.kargifk.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection successful");
+}).catch((err) => console.log('No connection'));
 
 app.get('/', (req, res) => {
     res.send("Hello world from the server");
